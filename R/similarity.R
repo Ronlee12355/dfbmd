cb91884c5dd0c7f3b95a78056065142d6cfbf337
Ronlee12355 <Ronlee12355@outlook.com>
2019-09-22 13:39:17 +0800

    first commit

---
+++ R/similarity.R
@@ -0,0 +1,8 @@
+#' @title Feature matrix of MLKNN
+#'
+#' @description Feature matrix of drug-indications (row: drug, column: indication)
+#' @name similarity
+#' @docType data
+#'
+#' @keywords dataset
+NULL
